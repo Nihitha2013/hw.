308bb8a8fb55f29c6334b70db501c9409bbc1196
Nihitha2013 <sowjanyay2020@gmail.com>
2025-04-16 16:21:30 +0530

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,7 @@
+char=input("Enter your character:")
+if len(char) == 1:
+    ascii_value= ord(char)
+    print("The ascii value of character:",ascii_value)
+else:
+    print("please enter only one chracter")
+
